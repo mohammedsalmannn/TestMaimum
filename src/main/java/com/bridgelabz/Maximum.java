dc1922c2975238caae57356c0843077ec3501e7d
mohammedsalmannn <rmdsalman001@gmail.com>
2021-06-05 20:34:49 +0530

    Added : Ability toFind Maximum Float

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -2,10 +2,16 @@ package com.bridgelabz;
 
 
 public class Maximum {
+    public static Float myFloat(Float firstValue, Float secondValue, Float thirdValue){
+        Float max = Math.max(firstValue,Math.max(secondValue,thirdValue));
+        return max;
+    }
+
     public static Integer maxInterge(Integer firstValue, Integer secondValue, Integer thirdValue) {
         int max = Math.max(firstValue,Math.max( secondValue, thirdValue));
      return max;
     }
+
 
 
     public static void main(String[] args) {
--- src/test/java/com/bridgelabz/MaximunTest.java
+++ src/test/java/com/bridgelabz/MaximunTest.java
@@ -19,4 +19,22 @@ public class MaximunTest {
     public void stMaximum_Integer_At_ThirdPoisiton_Gives_True() {
         Assert.assertEquals((Integer)3,Maximum.maxInterge(1,2,3));
     }
+
+    //.........FLoat.......//
+
+    @Test
+    public void testFloat_Integer_At_FirstPoisiton_Gives_True() {
+        Assert.assertEquals((Float)3.3f,Maximum.myFloat(3.3f,2.2f,1.1f));
+    }
+
+    @Test
+    public void testFloat_Integer_At_SecondPoisiton_Gives_True() {
+        Assert.assertEquals((Float)3.3f,Maximum.myFloat(1.1f,3.3f,2.2f));
+
+    }
+
+    @Test
+    public void testFloat_Integer_At_ThirdPoisiton_Gives_True() {
+        Assert.assertEquals((Float)3.3f,Maximum.myFloat(1.1f,2.2f,3.3f));
+    }
 }
