5de85dc50e028360516d3ec6835af222c6294af6
mohammedsalmannn <rmdsalman001@gmail.com>
2021-06-06 17:28:40 +0530

    ReFactor : Using Genric Methods to Find Maximum

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -2,28 +2,65 @@ package com.bridgelabz;
 
 
 public class Maximum {
-    public static Float myFloat(Float firstValue, Float secondValue, Float thirdValue){
-        Float max = Math.max(firstValue,Math.max(secondValue,thirdValue));
+    public static Float myFloat(Float firstValue, Float secondValue, Float thirdValue) {
+        Maximum maximum = new Maximum();
+        Float max = firstValue;
+        if (secondValue.compareTo(max) > 0)
+            max = secondValue;
+        if (thirdValue.compareTo(max) > 0)
+            max = thirdValue;
         return max;
     }
 
     public static Integer maxInterge(Integer firstValue, Integer secondValue, Integer thirdValue) {
-        int max = Math.max(firstValue,Math.max( secondValue, thirdValue));
-     return max;
+//        int max = Math.max(firstValue,Math.max( secondValue, thirdValue));
+//     return max;
+        Maximum maximum = new Maximum();
+        Integer max = firstValue;
+        if (secondValue.compareTo(max) > 0)
+            max = secondValue;
+        if (thirdValue.compareTo(max) > 0)
+            max = thirdValue;
+        return max;
     }
-
 
 
     public static void main(String[] args) {
         Maximum maximum = new Maximum();
     }
 
-    public static String myString(String fristSrting, String secondString, String thirdString) {
-        String max = fristSrting;
-        if(secondString.compareTo(max) > 0)
-            max = secondString;
-        if (thirdString.compareTo(max) > 0)
-            max = thirdString;
+    public static String myString(String firstValue, String secondValue, String thirdValue) {
+        String max = firstValue;
+        if (secondValue.compareTo(max) > 0)
+            max = secondValue;
+        if (thirdValue.compareTo(max) > 0)
+            max = thirdValue;
+
+        return max;
+    }
+    //.......GenricSs MethodSs......//
+
+    public static <E extends Comparable> E maximum(E[] arr) {
+        E temp = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (temp.compareTo(arr[i]) < 0)
+                temp = arr[i];
+        }
+        printMax(temp);
+        return temp;
+
+    }
+
+    private static <E extends Comparable> void printMax(E a) {
+        System.out.println("The Maximun Value is : " + a);
+    }
+        //..Genrics methods
+    public static<E extends Comparable> E GenMaximum( E firstValue, E secondValue, E thirdValue) {
+         E max = firstValue;
+        if (secondValue.compareTo(max) > 0)
+            max = secondValue;
+        if (thirdValue.compareTo(max) > 0)
+            max = thirdValue;
 
         return max;
     }
--- src/test/java/com/bridgelabz/MaximunTest.java
+++ src/test/java/com/bridgelabz/MaximunTest.java
@@ -6,12 +6,18 @@ import org.junit.Test;
 public class MaximunTest {
     @Test
     public void findMaximum_Integer_At_FirstPoisiton_Gives_True() {
+        //Assert.assertEquals((Integer) 3,Maximum.maxInterge(3,2,1));
+//        Maximum maximum = new Maximum();
+//        Integer arr[]={1,2,3};
+//        Integer result = Maximum.maximum(arr);
+//        Assert.assertEquals(java.util.Optional.of(3),java.util.Optional.of(result));
+        //Assert.assertEquals(Maximum.maximum,3);
         Assert.assertEquals((Integer) 3,Maximum.maxInterge(3,2,1));
+
     }
 
     @Test
     public void findMaximum_Integer_At_ScondPoisiton_Gives_True() {
-
         Assert.assertEquals((Integer) 3,Maximum.maxInterge(2,3,1));
     }
 
@@ -50,6 +56,29 @@ public class MaximunTest {
 
     @Test
     public void find_Maximum_Stirng_At_ThirdPoisiton_Gives_True() {
+       // String[] arr = {"Apple","peach","banana"};
         Assert.assertEquals("peach",Maximum.myString("Apple","banana","peach"));
+         //String result = Maximum.maximum(arr);
+        //Assert.assertEquals(java.util.Optional.of("peach"),java.util.Optional.of(result));
+    }
+        // .....Genric methods.....//
+
+    //.....Integer.....//
+    @Test
+    public void testMethod_for_Genric_method(){
+        Integer result = Maximum.GenMaximum(1,3,2);
+        Assert.assertEquals(java.util.Optional.of(3),java.util.Optional.of(result));
+    }
+    //.....Float.....//
+    @Test
+    public void testMethod_for_Genric_methodTwo() {
+        Float result = Maximum.GenMaximum(1.1f,3.3f,2.2f);
+        Assert.assertEquals(java.util.Optional.of(3.3f),java.util.Optional.of(result));
+    }
+    //.....string.....//
+    @Test
+    public void testMethod_for_Genric_methodThree() {
+        String result = Maximum.GenMaximum("Apple","Peach","Banana");
+        Assert.assertEquals(java.util.Optional.of("Peach"),java.util.Optional.of(result));
     }
 }
